b702ad8d966c386996c3ea637ac6587b5f7cee23
duffau <duffau@users.noreply.github.com>
2021-11-06 12:02:59 +0000

    Deploying to gh-pages from @ duffau/talks@3afff6227decda44dcf249cfce9434bf91d91f11 🚀

--- boosting/pydata-cph/simulation/ensemble_errors.py
+++ boosting/pydata-cph/simulation/ensemble_errors.py
@@ -12,59 +12,65 @@ MAX_ESTIMATORS = 200
 def error_rate(y, y_pred):
     return (y != y_pred).sum()/len(y)
 
+suffixes = ["", "noisy"]
 
-X = np.load("X_noisy.npy")
-y = np.load("y_noisy.npy")
+def join_underscore(*args):
+    return "_".join(str(arg) for arg in args if arg)
 
-# Train - test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8, random_state = SEED) 
+for suffix in suffixes:
+    x_path = f"data/{join_underscore('X', suffix) + '.npy'}"
+    y_path = f"data/{join_underscore('y', suffix) + '.npy'}"
+    print(f"Loading generated data '{x_path}'' and '{y_path}'...")
+    X = np.load(x_path)
+    y = np.load(y_path)
 
-# bag = BaggingClassifier(n_estimators=100)
-# random_forest = RandomForestClassifier(n_estimators=400)
-# adaboost = AdaBoostClassifier(n_estimators=400, learning_rate=1)
+    # Train - test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8, random_state = SEED) 
 
-def incremental_test_error(clf, X_test, y_test):
-    test_errors = []
-    y_proba = np.zeros(shape=(y_test.shape[0],2))
-    for i, estimator in enumerate(clf.estimators_):
-        y_proba += (estimator.predict_proba(X_test) - y_proba)/(i+1)
-        y_pred = np.argmax(y_proba, axis=1)*2 - 1
-        test_errors.append(error_rate(y_test, y_pred))
-    return test_errors
 
-test_errors = {}
-test_errors["single_tree"] = []
-print("Fitting single tree ...")
-for max_depth in range(1, MAX_ESTIMATORS):
-    clf = DecisionTreeClassifier(max_depth=max_depth)
+    def incremental_test_error(clf, X_test, y_test):
+        test_errors = []
+        y_proba = np.zeros(shape=(y_test.shape[0],2))
+        for i, estimator in enumerate(clf.estimators_):
+            y_proba += (estimator.predict_proba(X_test) - y_proba)/(i+1)
+            y_pred = np.argmax(y_proba, axis=1)*2 - 1
+            test_errors.append(error_rate(y_test, y_pred))
+        return test_errors
+
+    test_errors = {}
+    test_errors["single_tree"] = []
+    print("Fitting single tree ...")
+    for max_depth in range(1, MAX_ESTIMATORS):
+        clf = DecisionTreeClassifier(max_depth=max_depth)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        test_errors["single_tree"].append(error_rate(y_test, y_pred))
+
+
+    print("Fitting Bagging classifier ...")
+    clf = BaggingClassifier(n_estimators=MAX_ESTIMATORS, bootstrap=True, n_jobs=-1)
+    clf.fit(X_train, y_train)
+    test_errors["bagging"] = incremental_test_error(clf, X_test, y_test)
+
+    print("Fitting Random forest ...")
+    clf = RandomForestClassifier(n_estimators=MAX_ESTIMATORS, n_jobs=-1)
+    clf.fit(X_train, y_train)
+    test_errors["random_forest"] = incremental_test_error(clf, X_test, y_test)
+
+    print("Fitting AdaBoost ...")
+    clf = AdaBoostClassifier(n_estimators=MAX_ESTIMATORS)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    test_errors["single_tree"].append(error_rate(y_test, y_pred))
+    test_errors["adaboost"] = incremental_test_error(clf, X_test, y_test)
 
-
-print("Fitting Bagging classifier ...")
-clf = BaggingClassifier(n_estimators=MAX_ESTIMATORS, bootstrap=True, n_jobs=-1)
-clf.fit(X_train, y_train)
-test_errors["bagging"] = incremental_test_error(clf, X_test, y_test)
-
-print("Fitting Random forest ...")
-clf = RandomForestClassifier(n_estimators=MAX_ESTIMATORS, n_jobs=-1)
-clf.fit(X_train, y_train)
-test_errors["random_forest"] = incremental_test_error(clf, X_test, y_test)
-
-print("Fitting AdaBoost ...")
-clf = AdaBoostClassifier(n_estimators=MAX_ESTIMATORS)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-test_errors["adaboost"] = incremental_test_error(clf, X_test, y_test)
-
-
-fig1, ax = plt.subplots()
-for algo, _test_errors in test_errors.items():
-    ax.plot(_test_errors, label=algo)
-plt.legend()
-plt.title("Test error")
-plt.ylabel("Misclassification error rate")
-plt.xlabel("n trees/n leafs")
-plt.savefig("ensemble_test_errors_noisy.svg")
-plt.close()
+    plot_filename = f"plots/{join_underscore('ensemble_test_errors', suffix) + '.svg'}"
+    print(f"Plotting '{plot_filename}' ...")
+    fig1, ax = plt.subplots()
+    for algo, _test_errors in test_errors.items():
+        ax.plot(_test_errors, label=algo)
+    plt.legend()
+    plt.title("Test error")
+    plt.ylabel("Misclassification error rate")
+    plt.xlabel("n trees/n leafs")
+    plt.savefig(plot_filename)
+    plt.close()
