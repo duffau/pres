14be6962813cc182e420df3f0027b4433f716879
Christian Duffau-Rasmussen <duffau.christian@gmail.com>
2021-11-07 23:44:01 +0100

    plotting 2D example of simulation data

--- boosting/pydata-cph/simulation/gen_noisy_data.py
+++ boosting/pydata-cph/simulation/gen_noisy_data.py
@@ -6,11 +6,12 @@ from utils import gen_spherical_data
 N = 10000
 K = 10
 SEED = 123
+NOISE_PCT = 0.75
 
 X, y = gen_spherical_data(
     n=N, 
     k=K,
-    noise_pct_thershold=0.75,
+    noise_pct_thershold=NOISE_PCT,
     seed=SEED
 )
 
@@ -18,19 +19,28 @@ print("Saving X and y data...")
 np.save("data/X_noisy.npy", X)
 np.save("data/y_noisy.npy", y)
 
-# Plotting on reduced data
-n_max = 10000
+# Plotting on reduced example data
+N = 5000
+K = 2
+SEED = 123
+
+X, y = gen_spherical_data(
+    n=N, 
+    k=K,
+    noise_pct_thershold=NOISE_PCT,
+    seed=SEED
+)
+
 x1, x2 , *ignore = X.T
-x1, x2 = x1[0:n_max], x2[0:n_max]
-y = y[0:n_max]
 colors = np.array(["tab:blue", "tab:red"])
 fig1, ax = plt.subplots()
+fig1.set_size_inches(5.5, 5.5)
 ax.set_aspect('equal')
 y_color_indic = (y+1)//2
 ax.scatter(x1, x2, s=3, c=colors[y_color_indic])
-plt.title(f"2D slice of {K}D ball")
+plt.title("2D example of nested spheres")
 plt.xlabel("x1")
 plt.ylabel("x2")
-plt.savefig("plots/gen_noisy_data.png")
 plt.tight_layout()
+plt.savefig("plots/gen_noisy_data.png")
 plt.close()
