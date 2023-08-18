e568bfef1d5e2b79c622a4103f370fb4e6fffe30
duffau <duffau@users.noreply.github.com>
2023-08-18 07:30:16 +0000

    Deploying to gh-pages from @ duffau/talks@6176ec32bed25c15c72bd15683e6e7657565488e 🚀

--- stock-prices-talk/eff-market-hyp/analysis/get_data.py
+++ stock-prices-talk/eff-market-hyp/analysis/get_data.py
@@ -1,17 +1,30 @@
+import os
 import pandas as pd
 from io import StringIO
+import time
 
-from settings import ALPHA_VANTAGE_API_KEY, RAPIDAPI_API_KEY
-from apis import alphavantage as av
+# from settings import ALPHA_VANTAGE_API_KEY, RAPIDAPI_API_KEY
+# from apis import alphavantage as av
 from apis import yahoofinance as yf
-from apis import worldbank as wb
+# from apis import worldbank as wb
+# from apis import eodhistoricaldata as eod
+
+# API_KEY = os.getenv("EOD_API_KEY")
+# API_KEY = os.getenv("API_DOJO_API_KEY")
+DATE_KEY = "Date"
+DELAY = 5
 
 tickers = ["^GSPC", "TSLA", "^DJI", "GME", "HPE", "GOOG"]
 data_dir = "./data"
 for ticker in tickers:
     print(f"Getting {ticker}...")
+    
+    # csv = eod.get_historical_data(symbol=ticker, api_key=API_KEY)
     csv = yf.get_historical_data_v7(symbol=ticker)
     df = pd.read_csv(StringIO(csv))
-    df['Date'] = pd.to_datetime(df['Date'])
+    df[DATE_KEY] = pd.to_datetime(df[DATE_KEY])
+    
     df.set_index("Date", inplace=True)
     df.to_pickle(f"{data_dir}/{ticker}_prices.pickle")
+    print("Sleeping...")
+    time.sleep(DELAY)
--- stock-prices-talk/eff-market-hyp/apis/apis/yahoofinance.py
+++ stock-prices-talk/eff-market-hyp/apis/apis/yahoofinance.py
@@ -1,5 +1,7 @@
 import requests
 import time
+import csv
+from io import StringIO
 
 def get_historical_data(symbol, api_key):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data"
@@ -11,11 +13,24 @@ def get_historical_data(symbol, api_key):
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
         }
 
-    return requests.get(url, headers=headers, params=querystring).json()
+    json_resp = requests.get(url, headers=headers, params=querystring).json()
+    breakpoint()
+    price_records = json_resp["prices"]
+    output = StringIO()
+    writer = csv.DictWriter(output, fieldnames=price_records[0].keys())
+    writer.writeheader()
+    writer.writerows(price_records)
+    csv_string = output.getvalue()
+    return csv_string
+
 
 def get_historical_data_v7(symbol):                                                                                             
     url = "https://query1.finance.yahoo.com/v7/finance/download"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
     url = "/".join([url, symbol])
+    query_params = None
     query_params = {
         "period1":-1325635200,
         "period2": int(time.time()),
@@ -23,4 +38,4 @@ def get_historical_data_v7(symbol):
         "events": "history", 
         "includeAdjustedClose": "true"
     }
-    return requests.get(url, params=query_params).text
+    return requests.get(url, params=query_params, headers=headers).text
