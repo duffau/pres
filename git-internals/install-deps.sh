2588d5fc62f2e2a671e781a191d89c2427907a75
duffau <duffau@users.noreply.github.com>
2024-02-13 13:30:10 +0000

    Deploying to gh-pages from @ duffau/talks@1bf5d8a75916a39254c1b42f206f7b89cc733f3a 🚀

--- git-internals/install-deps.sh
+++ git-internals/install-deps.sh
@@ -1,12 +1,2 @@
-sudo apt-get install -y cabal-install
-cabal update
-wget https://github.com/owickstrom/pandoc-include-code/archive/refs/heads/master.zip
-unzip master.zip
-rm -f master.zip
-cd pandoc-include-code-master
-cabal configure
-cabal install
-cd .. && rm -rf ./pandoc-include-code-master
-export PATH=$PATH:~/.cabal/bin
-rm -rf dist
- 
+pipx install pandoc-codeblock-include
+
