ec3e75ba9ea6f10450cc306a84f08041f1bf039b
Christian Duffau-Rasmussen <duffau.christian@gmail.com>
2024-09-03 01:32:33 +0200

    move Finner analysis to separate folder

---
+++ sequence-tagging/analysis/crf/conll_2003/get_data.py
@@ -0,0 +1,9 @@
+import datasets
+import settings as sett
+
+datset = datasets.load_dataset(sett.DATASET_ID)
+
+
+cache_directory = sett.BASE_DATASET_DIR
+datset.save_to_disk(cache_directory)
+print(f"Sampled dataset saved to {cache_directory}")
---
+++ sequence-tagging/analysis/crf/conll_2003/settings.py
@@ -0,0 +1,6 @@
+DATASET_ID = "eriktks/conll2003"
+N_SAMPLE = None
+SEED = 42
+
+BASE_DATASET_DIR = f'./.data/base/{DATASET_ID}'
+FIT_DATASET_DIR = f'./.data/fit/{DATASET_ID}'
